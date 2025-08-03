08f4999578c741f89bca7c98142e4f82ad529a87
Filip Stępień <filip.stepien30@gmail.com>
2025-08-03 18:34:26 +0200

    feat: configure prisma & mongodb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import { Layout } from '@/components/Layout';
 import Providers from '@/components/Providers';
+import { prisma } from '@/db/prisma';
 
 type Props = {
     children: ReactNode;
@@ -14,8 +15,26 @@ export const metadata: Metadata = {
     title: 'App'
 };
 
+async function testDatabase() {
+    const id = '60cc9b0e001e3bfd00a6eddf';
+    const name = 'Created at ' + new Date().toLocaleString();
+    try {
+        await prisma.$connect();
+        await prisma.user.upsert({
+            where: { id },
+            update: { name },
+            create: { id, name }
+        });
+        console.log('Added test user.', name);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
 export default async function RootLayout({ children }: Props) {
     const session = await getServerSession();
+
+    await testDatabase();
 
     return (
         <html lang='en' {...mantineHtmlProps}>
