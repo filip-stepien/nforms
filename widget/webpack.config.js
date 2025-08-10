ec1b45cb344da1fb3452707be7f58d223173ab2f
Filip Stępień <filip.stepien30@gmail.com>
2025-08-10 16:43:52 +0200

    feat: lit widget config

---
+++ widget/webpack.config.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+        filename: 'widget.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    },
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devtool: 'source-map'
+};
+
+export default config;
